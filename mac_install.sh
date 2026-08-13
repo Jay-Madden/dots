1446fa01d379456682c10b247be6074b90e7f7e0
Jay Madden <jaymaddencox@gmail.com>
2026-08-13 10:52:33 -0500

    fix: add zsh plugins to install script

--- mac_install.sh
+++ mac_install.sh
@@ -10,6 +10,24 @@ error() {
     printf '\033[1;31mERROR\033[0m %s\n' "$*" >&2
 }
 
+install_zsh_plugin() {
+    local name="$1"
+    local url="$2"
+    local destination="$zsh_custom/plugins/$name"
+
+    if [[ -d "$destination/.git" ]]; then
+        return
+    fi
+
+    if [[ -e "$destination" ]]; then
+        error "Cannot install $name because $destination already exists."
+        exit 1
+    fi
+
+    info "Installing $name."
+    git clone --depth=1 "$url" "$destination"
+}
+
 repo_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 config_home="${XDG_CONFIG_HOME:-$HOME/.config}"
 zshenv="$HOME/.zshenv"
@@ -32,6 +50,26 @@ if [[ ! -x "$brew_bin" ]]; then
 fi
 
 "$brew_bin" bundle --file "$repo_dir/Brewfile"
+
+oh_my_zsh_dir="${ZSH:-$HOME/.oh-my-zsh}"
+zsh_custom="${ZSH_CUSTOM:-$oh_my_zsh_dir/custom}"
+
+if [[ ! -d "$oh_my_zsh_dir/.git" ]]; then
+    if [[ -e "$oh_my_zsh_dir" ]]; then
+        error "Cannot install Oh My Zsh because $oh_my_zsh_dir already exists."
+        exit 1
+    fi
+
+    info "Installing Oh My Zsh."
+    git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git "$oh_my_zsh_dir"
+fi
+
+mkdir -p "$zsh_custom/plugins"
+install_zsh_plugin fzf-tab https://github.com/Aloxaf/fzf-tab.git
+install_zsh_plugin zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions.git
+install_zsh_plugin zsh-completions https://github.com/zsh-users/zsh-completions.git
+install_zsh_plugin zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting.git
+install_zsh_plugin zsh-vi-mode https://github.com/jeffreytse/zsh-vi-mode.git
 
 if [[ ${XDG_CONFIG_HOME:-} == "$repo_dir" && ${ZDOTDIR:-} == "$repo_dir/zsh" ]]; then
     info "The dots environment is already configured."
